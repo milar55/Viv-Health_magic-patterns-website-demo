dcf51284fa7897be1d2353bf91b17cc3fb0fe7d2
Rudmila Rahman <rudmilarahman@gmail.com>
2026-03-08 20:29:17 -0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Viv-Health_magic-patterns-website-demo/',
   plugins: [react()],
 })
